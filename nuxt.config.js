e517a2b82f90396f400d0ff24ee16f2b6cf82a4d
kangrsyd <me@ngodink.com>
2022-10-29 21:54:58 +0700

    Fix meta config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,81 +1,94 @@
 import tailwindTypography from "@tailwindcss/typography";
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+	ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+	// Target: https://go.nuxtjs.dev/config-target
+	target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "Hi! I'm Rasyid Ridho",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content:
-          "Identity & Product Designer, Laravel & Vue.js Expert, Software Architect",
-      },
-      { name: "format-detection", content: "telephone=no" },
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Figtree&display=swap",
-      },
-    ],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "Hi! I'm Rasyid Ridho",
+		meta: [
+			{ charset: "utf-8" },
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				hid: "description",
+				name: "description",
+				content: "Identity & Product Designer, Laravel & Vue.js Expert, Software Architect",
+			},
+			{ name: "format-detection", content: "telephone=no" },
+		],
+		link: [
+			{
+				rel: "icon",
+				type: "image/x-icon",
+				href: "/favicon.ico",
+			},
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css2?family=Figtree&display=swap",
+			},
+		],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/tailwindcss
+		"@nuxtjs/tailwindcss",
+	],
 
-  tailwindcss: {
-    config: {
-      theme: {
-        extend: {
-          fontFamily: {
-            sans: "Figtree, Arial, ui-sans-serif, sans-serif, Apple Color Emoji, Segoe UI Emoji",
-          },
-        },
-      },
-      plugins: [tailwindTypography],
-    },
-  },
+	tailwindcss: {
+		config: {
+			theme: {
+				extend: {
+					fontFamily: {
+						sans: "Figtree, Arial, ui-sans-serif, sans-serif, Apple Color Emoji, Segoe UI Emoji",
+					},
+				},
+			},
+			plugins: [tailwindTypography],
+		},
+	},
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/pwa
-    "@nuxtjs/pwa",
-    // https://go.nuxtjs.dev/content
-    "@nuxt/content",
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/pwa
+		"@nuxtjs/pwa",
+		// https://go.nuxtjs.dev/content
+		"@nuxt/content",
+	],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: "en",
-    },
-  },
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		options: {
+			ogTitle: "Hi! I'm Rasyid Ridho",
+			ogDescription: "Identity & Product Designer, Laravel & Vue.js Expert, Software Architect",
+		},
+		icon: {
+			fileName: "me.png",
+		},
+		manifest: {
+			lang: "en",
+		},
+	},
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
+	// Content module configuration: https://go.nuxtjs.dev/config-content
+	content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 };
